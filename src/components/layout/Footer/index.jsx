0ddd280aea869aa786ae312a3c4d756513051c2b
y0unj1NoH <nohyoong711@gmail.com>
2024-09-26 16:09:05 +0900

    Feat. Footer 컴포넌트 추가

--- src/components/layout/Footer/index.jsx
+++ src/components/layout/Footer/index.jsx
@@ -5,7 +5,8 @@ import Text from "../../common/Text";
 const FooterContainer = styled.footer`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  align-items: center;
+  gap: 48px;
 
   width: 100%;
   padding: 16px 0;
@@ -13,7 +14,6 @@ const FooterContainer = styled.footer`
 
 const IconContainer = styled.div`
   display: flex;
-  align-items: flex-start;
   gap: 16px;
 `;
 const Footer = () => {
@@ -21,7 +21,7 @@ const Footer = () => {
     <FooterContainer>
       <IconContainer>
         <Icon.Github />
-        <Icon name="mail" size={60} color="#2C2A4A" />
+        <Icon.Default name="mail" size={60} color="#2C2A4A" />
       </IconContainer>
       <Text color="#2C2A4A">copyrightⓒ 2024 All rights reserved</Text>
     </FooterContainer>
